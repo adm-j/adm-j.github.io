fe33caecc61829042b3e75154166ac3a4b070b59
adam <personal@adam-jones.me>
2026-09-07 17:17:25 +0000

    updated config, changed some text for about page

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,6 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://adm-j.github.io',
-    base: '/adm-j.github.io',
     integrations: [mdx(), sitemap(), react()],
     fonts: [
         {
